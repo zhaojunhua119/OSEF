3e00ea4ec6724f499e94f3d19bf9ec2dfdab799c
zhahua <zhao, junhua>
2020-02-09 23:34:03 -0800

    added task4

--- task-4/solution-4.c
+++ task-4/solution-4.c
@@ -1,1 +1,24 @@
 /* Your code goes here */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT 1024
+
+
+int main(int argc, char *argv[]) {
+	char command[MAX_INPUT] = {};
+
+	while (1) {
+		scanf("%s", command);
+		int tokenidx = 0;
+		char *pch = strtok(command, " \t");
+		while (pch != NULL) {
+			if (tokenidx == 0 && strcmp("exit", pch) == 0)
+				exit(EXIT_SUCCESS);
+			printf("%s\n", pch);
+			pch = strtok(NULL, " \t");
+		}
+	}
+	return EXIT_SUCCESS;
+}
